475027e9d859fd8b67a0c977746158ef5f2633b9
Andy Liu <32084972+AndyLiuCodes@users.noreply.github.com>
2020-05-06 02:39:24 -0700

    added Steam scraping

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 
 const express = require("express");
 const path = require("path");
-const axios = require("axios");
-const cheerio = require("cheerio");
+const GameScrape = require('./Scripts/GameScrape.js');
 
-const app = express();
+const app = express((req, res)=>{
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ a: 1 }, null, 3));
+});
 const port = process.env.PORT || "8000";
 
 app.get("/", (req, res) => {
@@ -18,27 +20,12 @@ app.listen(port, () => {
 });
 
 
-async function fetchHTML(url){
-    try {
-        const { data } = await axios.get(url);
-
-        return cheerio.load(data);
-    }
-    catch{
-        console.log(`ERROR: An error occured when fetching URL ${url}`)
-    }
-}
 
 app.get("/hi", async (req, res) => {
-    const html = await fetchHTML("https://example.com");
-    console.log(`${html('h1')}`)
-    // console.log($);
-    // console.log("-------------------")
-    // // console.log(`Site HTML: ${$.html()} \n\n`);
-    // console.log(`first h1 tag: ${$('h1').text()}`);
-    // we can use $('css selector')
-    // const pText=$('.blessMe').text()
-    //=> HI
-    // res.send(pText);
-    res.send(`${html('h1')}`)
+    const results = await GameScrape.ScrapeSteam(2);
+    // console.log(result)
+    // res.send(JSON.stringify(result))
+
+    res.send(JSON.stringify(results));
+
 });
