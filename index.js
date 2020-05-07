4e68705b0bfe2efedb5d2854ae674e16de7b474f
Andy Liu <32084972+AndyLiuCodes@users.noreply.github.com>
2020-05-07 15:31:10 -0700

    completed insertion of game data into db

--- index.js
+++ index.js
@@ -2,15 +2,17 @@
 
 const express = require("express");
 const path = require("path");
-const GameScrape = require('./Scripts/GameScrape.js');
+const GameScrape = require('./Scripts/GameScrape');
+const Pool = require('./db/connection');
 
-const app = express((req, res)=>{
+const app = express((req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify({ a: 1 }, null, 3));
 });
 const port = process.env.PORT || "8000";
 
 app.get("/", (req, res) => {
+    //Get website available for the day
     res.status(200).send("Hello world");
     console.log("Hello World");
 });
@@ -22,10 +24,20 @@ app.listen(port, () => {
 
 
 app.get("/hi", async (req, res) => {
-    const results = await GameScrape.ScrapeSteam(2);
-    // console.log(result)
-    // res.send(JSON.stringify(result))
+    // const results = await GameScrape.ScrapeSteam(5);
+    const results = await GameScrape.UpdateGameDatabase();
 
+    for(let i = 0; i < results.length; i++){
+        let game = results[i];
+
+        let query = `insert into gamestorage values('${game.title}', '${game.imgURL}','${game.link}', '${game.platform}' ,${game.free},${game.discounted})`;
+
+        Pool.query(query, (err, res) =>{
+            if(err){
+                console.log(err)
+            }
+        });
+    }
     res.send(JSON.stringify(results));
 
 });
