2d9037a4f8ff0bbd6f6446119a088f52af2ca5f4
Andy Liu <32084972+AndyLiuCodes@users.noreply.github.com>
2020-05-07 04:32:30 -0700

    added the extraction of reddit links for games

--- Scripts/GameScrape.js
+++ Scripts/GameScrape.js
@@ -1,15 +1,16 @@
 const puppeteer = require('puppeteer');
 const cheerio = require("cheerio");
-// async function fetchHTML(url){
-//     try {
-//         const { data } = await axios.get(url);
 
-//         return cheerio.load(data);
-//     }
-//     catch{
-//         console.log(`ERROR: An error occured when fetching URL ${url}`);
-//     }
-// }
+async function FetchHTML(url){
+    try{
+        const {data} = await axios.get(url);
+        
+        return data;
+    }
+    catch{
+        console.error(`ERROR: An error occured when trying to fetch url: ${url}`)
+    }
+}
 
 function extractSteamItems(){
     const extractedElements = document.querySelectorAll('#search_result_container > #search_resultsRows > a');
@@ -22,25 +23,49 @@ function extractSteamItems(){
 }
 
 function extractSteamInfo(game){
+    //Rating
+    //Categories
+
     const selector = cheerio.load(game.data);
     
-    const title = selector('.responsive_search_name_combined > .search_name > .title')
+    var title = selector('.responsive_search_name_combined > .search_name > .title')
         .text()
         .trim();
     
-    const releaseDate = selector('.responsive_search_name_combined > .search_released')
+    const price = selector('.responsive_search_name_combined > .search_price_discount_combined > .search_price')
+        .children().remove().end()
+        .text()
+        .trim();
+    
+    var discountPercentage = selector('.search_price_discount_combined > .search_discount > span')
         .text()
         .trim();
 
-    const price = selector('.responsive_search_name_combined > .search_price_discount_combined')
-        .text()
-        .trim();
-    
     const imgURL = selector('.search_capsule > img').attr('src');
 
     const link = game.link;
 
-    return {title, releaseDate, price, imgURL, link};
+    const platform = "steam";
+
+    var free = true;
+
+    if(!price.includes("Free")){
+        free = false;
+    }
+
+    var discounted;
+
+    if(discountPercentage == "" || discountPercentage == null){
+        title = title + " " + price;
+        discounted = false;
+    }
+    else{
+        title = title + " " + `(${price}/${discountPercentage})`
+        discounted = true;
+    }
+    
+    
+    return {title, imgURL, link, platform, free, discounted};
 }
 
 async function scrapeInfinitePage(
@@ -64,7 +89,7 @@ async function scrapeInfinitePage(
 }
 
 //STEAM
-var ScrapeSteam = async (numItems) => {
+var ScrapeSteam = async (numItems, url) => {
 
     const browser = await puppeteer.launch({
         headless: true,
@@ -74,7 +99,7 @@ var ScrapeSteam = async (numItems) => {
     const steamPage = await browser.newPage()
     steamPage.setViewport({width: 1920, height: 1080});
     
-    const steamURL = "https://store.steampowered.com/search/?sort_by=Reviews_DESC&maxprice=free";
+    const steamURL = "https://store.steampowered.com/search/?sort_by=Reviews_DESC&maxprice=6";
     
     await steamPage.goto(steamURL);
     
@@ -82,29 +107,80 @@ var ScrapeSteam = async (numItems) => {
 
     await browser.close();
     
-    const steamResults = steamGames.map((ele) => {
+    var steamResults = steamGames.map((ele) => {
         return extractSteamInfo(ele);
     });
     
     return steamResults;
 }
 
+function extractRedditItems(){
+    document.querySelectorAll(".linkflair-expired").forEach((a) =>{
+        a.remove()
+    })
+
+    const extractedElements = document.querySelectorAll('#siteTable > .link');
+    
+    const items = [];
+    for (let element of extractedElements) {
+        //title, imgurl, link, platform, free, discounted
+        items.push({data: element.innerHTML, link: element.href});
+    }
+    return items;   
+}
+
+function extractRedditInfo(game){
+
+}
 //REDDIT
-async function ScrapeReddit(){
+var ScrapeReddit = async (numPages, url) => {
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
 
+    const redditPage = await browser.newPage()
+    redditPage.setViewport({width: 1920, height: 1080});
+    
+    const redditURL = "https://old.reddit.com/r/GameDeals/hot/";
+    
+    await redditPage.goto(redditURL);
+    var allGameList = []
+    //extract items
+
+    for(let i = 0; i < 2; i++){
+        items = await redditPage.evaluate(extractRedditItems);
+        allGameList = await allGameList.concat(items)
+        
+        var href = await redditPage.$eval("#siteTable > div.nav-buttons > span > span > a", a => a.getAttribute('href'));
+        await redditPage.goto(href)        
+    }
+
+    await browser.close();
+    
+    var redditResults = redditResults.map((ele) => {
+        return extractRedditInfo(ele)
+    })
+    console.log(allGameList.length)
+    //process items
+
+    //return items
 }
 
-//EPIC GAMES
-async function ScrapeEpic(){
 
-}
+
 
 
 //ITCH.IO
-async function ScrapeItch(){
+// async function ScrapeItch(){
+
+// }
+
+function UpdateGameDatabase(){
 
 }
 
 module.exports = {
-    ScrapeSteam: ScrapeSteam
+    ScrapeSteam: ScrapeSteam,
+    ScrapeReddit: ScrapeReddit
 }
